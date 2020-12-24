0ffe456b66ae3dfc4eb6607868c24a27f716d54c
nagaraj <nagarajofficial143@gmail.com>
2020-12-25 00:32:39 +0530

    djs:

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const Biodata = require('./models/biodata');
 
 
 const app = express();
-app.use(bodyParser.json());
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
  });
+app.use(bodyParser.json());
+
+
 
 
 app.post('/getdata', mongoPractice.createBiodata);
