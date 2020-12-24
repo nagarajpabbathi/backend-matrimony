3269ea9e75a0d42b9fa9a31f904ebb19419dc32d
nagaraj <nagarajofficial143@gmail.com>
2020-12-25 00:37:53 +0530

    djsklkijl

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const Biodata = require('./models/biodata');
 
 const app = express();
 
-app.use('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use('*', function(req, response, next) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
  });
 app.use(bodyParser.json());
