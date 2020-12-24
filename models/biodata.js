cfaff1e8bd6153537fc3be10bcd8601dccf43101
nagaraj <nagarajofficial143@gmail.com>
2020-12-24 23:28:14 +0530

    make it better

---
+++ models/biodata.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const biodataSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    gender: { type: String, required: true },
+    dob: { type: Date, required: true },
+    tob: { type: String, required: true },
+    height: { type: Number, required: true },
+    qualify: { type: String, required: true },
+    jobDetails: { type: String, required: true },
+    jobType: { type: String, required: true },
+    company: { type: String, required: true },
+    salary: { type: String, required: true },
+    caste: { type: String, required: true },
+    gothram: { type: String, required: true },
+    father: { type: String, required: true },
+    fatheroccu: { type: String, required: true },
+    mother: { type: String, required: true },
+    motheroccu: { type: String, required: true },
+    sibilings: { type: String, required: true },
+    district: { type: String, required: true },
+    pincode: { type: Number, required: true },
+    photo1: { type: String, required: true },
+    photo2:{type:Number}
+    
+})
+
+module.exports=mongoose.model('biodata',biodataSchema)
---
+++ server.js
@@ -0,0 +1,30 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongooseClient = require('mongoose')
+
+const mongoPractice = require('./mongoose')
+
+const Biodata = require('./models/biodata');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.post('/getdata', mongoPractice.createBiodata);
+
+app.get('/getdata',async (req, res) => {
+    let data = await Biodata.find();
+    
+   
+    res.send(data);
+
+})
+
+app.listen(5000, console.log('server running on port 5000'));
