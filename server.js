555641da157fc75d6cb858c0d0e125506d124dd8
nagaraj <nagarajofficial143@gmail.com>
2020-12-25 00:17:57 +0530

    ksdlfa

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const Biodata = require('./models/biodata');
 const app = express();
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
-  });
+ });
 
 
 app.post('/getdata', mongoPractice.createBiodata);
