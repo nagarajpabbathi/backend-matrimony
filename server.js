bbce6dd11e48342884e9dda723e329a665f3298e
nagaraj <nagarajofficial143@gmail.com>
2021-01-30 00:09:47 +0530

    lsljf

--- server.js
+++ server.js
@@ -386,6 +386,20 @@ app.get('/ngetdata', async(req, res,next) => {
         }
     });
 })
+app.post('/getdata', async (req, res, next) => {
+    if (req.body.username == 'username' && req.body.password == 'password') {
+        const data = await Biodata.find({verified:true},{"name":1,"qualify":1,"dob":1,"district":1,"photo1":1,"search":1,"caste":1,"phone":1}, (err, data) => {
+            if (err) {
+                res.send(err)
+            }
+            else {
+                res.send(data);
+            }
+        });
+    } else {
+        res.send('invalid credentials')
+    }
+})
 ////////////////////////////////////////////////////////////
 
 /////////////////////////MAKE VERIFY///////////////////////////////
